0074d1bb53cbd67e019f0d5ea2caa426dfec3c19
Noah <376030434@qq.com>
2018-02-23 14:42:18 +0800

    Creat Project

---
+++ Child/ChildTests/ChildTests.swift
@@ -0,0 +1,36 @@
+//
+//  ChildTests.swift
+//  ChildTests
+//
+//  Created by Noah on 2018/2/23.
+//  Copyright © 2018年 NH. All rights reserved.
+//
+
+import XCTest
+@testable import Child
+
+class ChildTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
